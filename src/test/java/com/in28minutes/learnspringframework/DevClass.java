986719875e122435bc8788f9966156d969e28785
VeekeshSingh <singhofficial2408@gmail.com>
2023-09-26 10:56:32 +0530

    Updated Commit

--- src/test/java/com/in28minutes/learnspringframework/DevClass.java
+++ src/test/java/com/in28minutes/learnspringframework/DevClass.java
@@ -3,5 +3,6 @@ package com.in28minutes.learnspringframework;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
+        //Changes done for commit
     }
 }
