b07a707e11dad4ef429bf3d82f2d01a4708dffba
VeekeshSingh <singhofficial2408@gmail.com>
2023-09-27 11:19:05 +0530

    Notes

---
+++ src/test/java/com/in28minutes/learnspringframework/NoteOfSpringFramework.java
@@ -0,0 +1,42 @@
+package com.in28minutes.learnspringframework;
+
+public class NoteOfSpringFramework {
+    /*
+      1. Spring Framework contains multiple Spring Modules:
+
+         a. Fundamental Features: Core (IOC Container, Dependency Injection, AutoWiring..)
+         b. Web: Spring MVC etc (Web applications, REST API)
+         c. Web Reactive: Spring WebFux etc.
+         d. Data Access: JDBC, JPA etc.
+         f. Integration: JMS etc,
+         g. Testing: Mock Objects, Spring MVC Test etc.
+
+      2. Application architectures evolve continuously.
+         web>REST API>Microservices>Cloud>...
+
+      3. Spring evolves through Spring projects:
+
+         a. First Project: Spring Framework.
+         b. Spring Security: Secure your web application or REST API or microservices.
+         c. Spring Data: Integrate the same way with different type of databases: NoSql and Relational.
+         d. Spring Integration: Address challenges with integration with other applications.
+         e. Spring Boot: Popular framework to build microservices.
+         f. Spring Cloud: Build cloud native applications.
+
+      4. Hierarchy: Spring Projects> Spring Framework> Spring Modules.
+
+      5. Why is Spring Eco System popular?
+
+         a. Loose Coupling: Spring manages creation and wiring of beans and dependencies.
+            Makes it easy to build loosely coupled applications.
+            Makes writing unit testing easy!(Spring uit testing).
+
+         b. Reduced boilerplate code: Focus only on business logic.
+            example: no need for exception handling in each method.
+
+         c. Architectural Flexibility: you can pick and choose which one to use.
+
+         d. Evolution with time: Microservices and cloud.
+
+     */
+}
